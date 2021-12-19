f7990c3886b9437b7b37ade7bacaa75a8db930b9
SnehaMane17 <69333961+SnehaMane17@users.noreply.github.com>
2021-12-19 11:12:08 +0530

    Add files via upload

---
+++ Java_Setup.ps1
@@ -0,0 +1,10 @@
+﻿
+$variable_name = Read-Host -Prompt 'variable Name'
+Write-Host $variable_name
+
+$variable_value = Read-Host -Prompt 'Variable Value'
+Write-Host $variable_value
+
+[Environment]::GetEnvironmentVariable($variable_name, 'User')
+
+[Environment]::SetEnvironmentVariable($variable_name, $variable_value, 'User')
---
+++ Update_Records.ps1
@@ -0,0 +1,13 @@
+﻿$Excel = New-Object -ComObject Excel.application
+$Excel.Visible = $true
+$Workbook = $Excel.Workbooks.Open("C:\Users\sneha\file_example1_XLSX.xlsx")
+$Worksheet = $Workbook.Worksheets.Item(1)
+$Worksheet.Activate()
+$range = $Worksheet.Range("E2","E10")
+$range.clear()
+$Worksheet.Range("E2","E10") = 'India'
+$Workbook.Save()
+$Excel.Quit()
+
+
+
