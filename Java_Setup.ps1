f18669c9b23a05178684dc9e9780576c4b9ef9c3
SnehaMane17 <69333961+SnehaMane17@users.noreply.github.com>
2021-12-20 21:14:36 +0530

    Update Path

--- Java_Setup.ps1
+++ Java_Setup.ps1
@@ -1,10 +1,10 @@
 ﻿
 $variable_name = Read-Host -Prompt 'variable Name'
 Write-Host $variable_name
-
 $variable_value = Read-Host -Prompt 'Variable Value'
 Write-Host $variable_value
-
 [Environment]::GetEnvironmentVariable($variable_name, 'User')
-
 [Environment]::SetEnvironmentVariable($variable_name, $variable_value, 'User')
+$path = [Environment]::GetEnvironmentVariable('Path', 'User')
+$newpath = $path + ';' + $variable_value + '\bin'
+[Environment]::SetEnvironmentVariable("Path", $newpath, 'User')
